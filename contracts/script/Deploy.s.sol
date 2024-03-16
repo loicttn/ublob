1463340292a185b6ea78b3fbc104445fe5481293
Gauthier <legrangm@gmail.com>
2024-03-16 11:23:34 +0000

    feat: contract deployment

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {uBlob} from "../src/uBlob.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        uBlob ublob = new uBlob(payable(address(msg.sender)));
+        console2.log("uBlob deployed at", address(ublob));
     }
 }
